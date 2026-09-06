0294935e0bae0e7a67221be7e46f480b67dfbb4f
ravi teja <pokalaraviteja884@gmail.com>
2026-09-06 09:37:07 +0530

    Update 19 08 Python.py

--- 19 08 Python.py
+++ 19 08 Python.py
@@ -64,7 +64,7 @@ n=int(input())
 for i in range(m,n+1):
     if i%2!=0:
         print(i)
-'''14)m=int(input())
+m=int(input())
 n=int(input())
 even=0
 odd=0
@@ -74,8 +74,8 @@ for i in (m,n + 1):
     else:
         odd+=1
 print('even =',even)  
-print('odd=',odd)'''
-'''15,16)n=input()
+print('odd=',odd)
+n=input()
 rev=''
 for char in n:
     rev=char+rev
@@ -84,24 +84,20 @@ if rev==rev:
     print('palindrome')
 else:
     print('not palindrome')
-    '''
-'''17)n=int(input())
+n=int(input())
 sum=0
 while n>0:
     sum+=n%10
     n//=10
 print(sum)
-    '''
 
-'''18)n=int(input())
+n=int(input())
 mul=1
 while n>0:
     mul*=n%10
     n//=10
 print(mul)
 
- '''
-''' 19)
 number=int(input())
 
 original_number = number
@@ -123,8 +119,8 @@ if digit_sum == original_number:
     print(" Armstrong number")
 else:
     print(" not an Armstrong number")
-'''
-'''21)n=int(input())
+
+n=int(input())
 temp=n
 reversed_num = 0
 while n>0:
@@ -134,16 +130,15 @@ while n>0:
 if reversed_num==temp:
     print('Palindrome')
 else:
-    print('not Palindrome')   '''
-'''22)s = input()
+    print('not Palindrome')  
+s = input()
 count = 0
 for ch in s:
     if ch in "aeiou":
         count = count + 1
 
 print(count)
-'''
-'''23)s = "apple"
+s = "apple"
 count = 0
 
 for ch in s:
@@ -151,8 +146,7 @@ for ch in s:
         count = count + 1
 
 print(count)
-'''
-'''24)s = "apple"
+s = "apple"
 vowels = 0
 consonants=0
 
@@ -164,8 +158,7 @@ for ch in s:
   
 print('vowels:',vowels,'consonants:',consonants)
 
-'''
-'''25)n=int(input())
+n=int(input())
 sum=0
 for i in range(1,n):
     if n%i==0:
@@ -174,8 +167,7 @@ if sum==n:
     print('Perfect number')
 else:
     print('not Perfect number')
-'''
-'''26)n = int(input())
+n = int(input())
 square = n * n
 sum = 0
 
@@ -189,8 +181,7 @@ if sum == n:
 else:
     print("Not Neon number")
 
-    '''
-'''27)n = int(input())
+n = int(input())
 temp = n
 sum = 0
 
@@ -207,8 +198,7 @@ while n > 0:
 if sum == temp:
     print("Strong number")
 else:
-    print("Not Strong number")'''
-'''28)
+    print("Not Strong number")
 n = int(input())
 temp = n
 sum = 0
@@ -222,8 +212,7 @@ if temp % sum == 0:
     print("Harshad number")
 else:
     print("Not Harshad number")
- '''
-'''29)
+
 n=int(input())
 a,b=0,1
 for i in range(n):
@@ -231,8 +220,7 @@ for i in range(n):
     c=a+b
     a=b
     b=c
-    '''
-'''30)
+
 n =int(input())
 square = n * n
 sum = 0
@@ -246,6 +234,5 @@ if sum == n:
     print("Neon number")
 else:
     print("Not Neon number")
-'''
 
 
